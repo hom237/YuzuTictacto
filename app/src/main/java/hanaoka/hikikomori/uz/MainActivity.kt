ae68826f577403c9bf4017a23f01b218039a069b
hom237 <jinsic4568@gmail.com>
2024-06-06 00:35:15 +0900

    open

--- app/src/main/java/hanaoka/hikikomori/uz/MainActivity.kt
+++ app/src/main/java/hanaoka/hikikomori/uz/MainActivity.kt
@@ -1,20 +1,31 @@
 package hanaoka.hikikomori.uz
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import hanaoka.hikikomori.uz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: GridviewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val arrat = mutableListOf(ButtonStatus(-1),ButtonStatus(-1),ButtonStatus(-1),ButtonStatus(-1),ButtonStatus(-1),ButtonStatus(-1),ButtonStatus(-1),ButtonStatus(-1),ButtonStatus(-1))
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = GridviewAdapter()
+        adapter.setItems(arrat)
+        binding.gridView.adapter = adapter
+
+
     }
 }
